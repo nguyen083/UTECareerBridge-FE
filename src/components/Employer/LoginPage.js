ad983d82ccf0085861d31611dc5be92397c65b37
nguyen083 <billbatri085@gmail.com>
2024-07-11 23:58:58 +0700

    settup project

---
+++ src/components/Employer/LoginPage.js
@@ -0,0 +1,10 @@
+import { TbBackground } from "react-icons/tb";
+const LoginPage = () => {
+    return (
+        <div className="login-page">
+            
+        </div>
+
+    );
+}
+export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginPage from './components/Employer/LoginPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<LoginPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
