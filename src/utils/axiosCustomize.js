bcdc74745e646d4548e180ba9dc26169d340798b
nguyen083 <billbatri085@gmail.com>
2024-09-14 14:28:27 +0700

    CB-01| Interceptor for refresh token

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+
 const instance = axios.create({
     baseURL: 'http://localhost:8080/api/v1/',
 });
 
+
+//instance.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
     // Do something before request is sent
@@ -11,14 +14,31 @@ instance.interceptors.request.use(function (config) {
     // Do something with request error
     return Promise.reject(error);
 });
+const refreshToken = async () => {
+    return await axios.post('http://localhost:8080/api/v1/auth/refresh');
+}
+instance.setToken = (token) => {
 
+    console.log("token: ", token)
+    // localStorage.setItem('accessToken', token.accessToken);
+    // document.cookie = `refreshToken=${token.refreshToken}`;
+}
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response;
-}, function (error) {
+}, async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        const rs = await refreshToken();
+        instance.setToken(rs.data);
+        // console.log(rs.data)
+        // originalRequest.headers['Authorization'] = `Bearer ${rs.data.accessToken}`;
+        // return instance(originalRequest);
+    }
     // Do something with response error
     return error && error.response && error.response.data
         ? error.response.data : Promise.reject(error);
