b43ef82eefeae2c928c25ce942d7f51fac4448ba
nguyen083 <billbatri085@gmail.com>
2025-04-25 18:57:49 +0700

    CB-10 | Redirect the user to the login page if they attempt to comment without being logged in.

--- src/App.jsx
+++ src/App.jsx
@@ -386,15 +386,15 @@ const App = () => {
                         element={<PostListAdmin />}
                       />
                       {/* <Route path='/forum/create' element={<CreatePostPage />} /> */}
+                      <Route
+                        path="/forums/:forumId/topics/:topicId/posts/:postId"
+                        element={<PostDetail />}
+                      />
                     </Route>
                     <Route element={<ViewLayout width="100%" />}>
                       <Route
                         path="/forums/:forumId/topics/:topicId/posts"
                         element={<PostList />}
-                      />
-                      <Route
-                        path="/forums/:forumId/topics/:topicId/posts/:postId"
-                        element={<PostDetail />}
                       />
                     </Route>
                   </Route>
