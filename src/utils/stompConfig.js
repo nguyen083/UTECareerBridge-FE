0c84e0bb3bf59613ee7c69f9328d0f87eb91e814
nguyen083 <billbatri085@gmail.com>
2025-06-09 01:55:22 +0700

    CB-17 | update stompConfig

--- src/utils/stompConfig.js
+++ src/utils/stompConfig.js
@@ -25,7 +25,7 @@ export const connectStomp = (onConnected, onError, requireToken = false) => {
     return null;
   }
   // Tạo instance mới của SockJS với endpoint /ws
-  const socket = new SockJS("/ws");
+  const socket = new SockJS("https://whine2202.zapto.org/server/ws");
 
   // Chuẩn bị headers dựa vào việc có yêu cầu token hay không
   const connectHeaders = {};
